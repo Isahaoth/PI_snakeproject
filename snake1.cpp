34a9607fe27d940d03bd5d40d8d580e6c6746b7d
Alexa <wozniakola76@gmail.com>
2025-12-17 20:06:07 +0100

    Bug fixes: ver 0.1
    - Dodano define, aby dzialal zwykly int main;
    - Zmieniono deklaracje biblioteki SDL, aby pokladala sie z zainstalowana SDL2;
    - Dodano zmienne globalne odpowiadajace za wymiary okna - rozmiar snake'a od teraz skaluje sie z wymiarami okna.

--- snake1.cpp
+++ snake1.cpp
@@ -1,8 +1,12 @@
-#include <SDL.h>
+#define SDL_MAIN_HANDLED // pozwala na uzywanie zwyklej funkcji main
+#include <SDL2/SDL.h> //wieksze szanse ze program zadziala
 #include <iostream>
-#undef main 
 
 using namespace std; 
+
+//wymiary okna
+int WINDOW_WIDTH = 1280;
+int WINDOW_HEIGHT = 720;
 
 enum Direction
 {
@@ -12,27 +16,24 @@ enum Direction
     UP = 3,
 };
 
-int main(int argc, char* argv[]) 
+int main() 
 {
-    // 1. Inicjalizacja z obsługą błędu
+    // Inicjalizacja z obsługą błędu
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         cerr << "Błąd inicjalizacji SDL: " << SDL_GetError() << endl;
         return 1;
     }
 
-    // 2. Tworzenie okna
-    SDL_Window* window = SDL_CreateWindow("Test Renderowania",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED, 
-                                          1000, 1000, 
-                                          SDL_WINDOW_SHOWN);
+    // Tworzenie okna
+    SDL_Window* window = SDL_CreateWindow("Indiana Snake",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    
     if (!window) {
         cerr << "Błąd tworzenia okna: " << SDL_GetError() << endl;
         SDL_Quit();
         return 1;
     }
 
-    // 3. Wymuszenie Akceleracji (SDL_RENDERER_ACCELERATED) i VSync (SDL_RENDERER_PRESENTVSYNC)
+    // Wymuszenie Akceleracji (SDL_RENDERER_ACCELERATED) i VSync (SDL_RENDERER_PRESENTVSYNC)
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 
                                                 SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     
@@ -47,7 +48,7 @@ int main(int argc, char* argv[])
     SDL_Event e;
     bool running = true;
     int dir = RIGHT; // Startowy ruch w prawo
-    SDL_Rect head {500, 500, 10, 10};
+    SDL_Rect head {WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 20, 20};
 
     while(running)
     {
@@ -63,17 +64,17 @@ int main(int argc, char* argv[])
         }
         switch(dir)
         {
-            case DOWN:  head.y += 10; break;
-            case UP:    head.y -= 10; break;
-            case LEFT:  head.x -= 10; break;
-            case RIGHT: head.x += 10; break;
+            case DOWN:  head.y += 20; break;
+            case UP:    head.y -= 20; break;
+            case LEFT:  head.x -= 20; break;
+            case RIGHT: head.x += 20; break;
         }
 
-        // 4. Renderowanie (RÓŻOWA kropka na CIEMNOSZARYM tle)
+        // Renderowanie (rozowa kropka na ciemnoszarym tle)
         SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255); // Ciemnoszary
         SDL_RenderClear(renderer);
 
-        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255); // RÓŻOWY
+        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255); // Rozowy
         SDL_RenderFillRect(renderer, &head); 
 
         SDL_RenderPresent(renderer);
@@ -81,10 +82,11 @@ int main(int argc, char* argv[])
         SDL_Delay(25);
     }
 
-    // 5. Sprzątanie
+    // Sprzątanie
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     
     return 0;
 }
+
